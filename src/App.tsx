a8c80ac2372c0427137ba8ddf709a553591f22e3
nomadHash <badsoap141@gmail.com>
2022-08-22 17:53:29 +0900

    add initailize setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Global } from "@emotion/react";
+import { ReactElement } from "react";
+import resetCss from "@styles/globalStyle";
+
+const App = (): ReactElement => {
+  return (
+    <div>
+      <Global styles={resetCss} />
+    </div>
+  );
+};
+
+export default App;
